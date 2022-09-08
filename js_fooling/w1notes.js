6ce38a75148f590c5e5177df7f2d445dbeb0216e
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-09-08 14:04:50 -0400

    Do the thing
    I did a thing

---
+++ js_fooling/w1notes.js
@@ -0,0 +1,6 @@
+let myName = "A. Name";
+console.log(myName);
+
+myName = "Same variable, different name.";
+console.log(myName);
+
---
+++ labs/lab2/js/app.js
@@ -0,0 +1,44 @@
+activeInstrument = 0;
+
+class Instrument {
+    constructor(loudness, family, playVerb, over) {
+        this.loudness = loudness;
+        this.family = family;
+        this.playVerb = playVerb;
+        this.synth = new Tone.Synth().toDestination();
+        this.over = playNext;
+    }
+
+    playInstrument(duration) {
+        this.synth.triggerAttackRelease("C4", duration);
+        console.log(this.family + " " + this.playVerb + " at " + this.loudness + " loudness");
+        setTimeout(this.over, 1000);
+    }
+}
+
+class Woodwind extends Instrument {
+    constructor(loudness) {
+        super(loudness, "Woodwind", "play");
+    }
+}
+class Percussion extends Instrument {
+    constructor(loudness) {
+        super(loudness, "Percussion", "bam");
+    }
+}
+class String extends Instrument {
+    constructor(loudness) {
+        super(loudness, "String", "twang");
+    }
+}
+
+function playNext() {
+    instruments[activeInstrument].playInstrument(.5);
+    activeInstrument++;
+}
+
+flute = new Woodwind(10);
+drum = new Percussion(20);
+guitar = new String(30);
+const instruments = [flute, drum, guitar];
+console.log(instruments);
